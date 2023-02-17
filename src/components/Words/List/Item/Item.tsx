91beb21a8edf01900e4cd7a8bfde37a22325fbf2
Andrew Taranenko <krol2006@bigmir.net>
2023-02-17 21:22:34 +0100

    Some changes

--- src/components/Words/List/Item/Item.tsx
+++ src/components/Words/List/Item/Item.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import bufferToWav from "audiobuffer-to-wav";
 
 import { SvgPauseIcon, SvgPlayIcon } from "~/icons";
 import { TProps } from "./types";
@@ -11,29 +12,42 @@ const Item: React.FC<TProps> = ({
   transcription,
   word,
 }) => {
+  const [audio] = useState<HTMLAudioElement>(new Audio());
   const [isPlaying, togglePlaying] = useState<boolean>(false);
 
   const handleAudioPlay = async (): Promise<void> => {
-    const audioContext = new window.AudioContext();
+    const reader = new FileReader();
+    const audioContext = new AudioContext();
     const response = await axios.get(audioLink, {
       responseType: "arraybuffer",
     });
 
-    togglePlaying(true);
-    audioContext.decodeAudioData(
-      response["data"],
-      (buffer: AudioBuffer): void => {
-        const source = audioContext.createBufferSource();
+    reader.onloadend = (): void => {
+      audio.setAttribute("src", reader["result"] as string);
+      audio.play();
+    };
 
-        source.buffer = buffer;
-        source.connect(audioContext.destination);
-        source.start();
-        source.onended = (): void => {
-          togglePlaying(false);
-        };
-      }
-    );
+    if (response && response["data"]) {
+      audioContext.decodeAudioData(
+        response["data"],
+        (buffer: AudioBuffer): void => {
+          const wav: ArrayBuffer = bufferToWav(buffer);
+          const blob: Blob = new Blob([new DataView(wav)], {
+            type: "audio/wav",
+          });
+
+          reader.readAsDataURL(blob);
+        }
+      );
+    }
+    togglePlaying(true);
   };
+
+  useEffect((): void => {
+    audio.onended = () => {
+      togglePlaying(false);
+    };
+  }, []);
 
   return (
     <tr className={styles.tr}>
