dadfd5fd642c9c520f9981918f47846b04acaecd
Andrew Taranenko <krol2006@bigmir.net>
2023-02-18 02:59:39 +0100

    Some changes

--- src/components/Words/List/Item/Item.tsx
+++ src/components/Words/List/Item/Item.tsx
@@ -6,11 +6,6 @@ import { SvgPauseIcon, SvgPlayIcon } from "~/icons";
 import { TProps } from "./types";
 import styles from "./item.module.css";
 
-// function unlock() {
-//   console.log("unlocking");
-
-// }
-
 const Item: React.FC<TProps> = ({
   audioLink,
   translation,
@@ -21,46 +16,48 @@ const Item: React.FC<TProps> = ({
 
   const handleAudioPlay = async (): Promise<void> => {
     if ("AudioContext" in window || "webkitAudioContext" in window) {
-      // @ts-ignore
       const AudioContext = window.AudioContext || window.webkitAudioContext;
+      const reader = new FileReader();
       const response = await axios.get(audioLink, {
         responseType: "arraybuffer",
       });
       const audioContext = new AudioContext();
-      const gainNode = audioContext.createGain();
 
-      gainNode.gain.value = 1;
+      reader.onloadend = async (): Promise<void> => {
+        if (reader["result"]) {
+          const soundUrl = reader["result"] as string;
+          const gainNode = audioContext.createGain();
+          const response = await axios.get(soundUrl, {
+            responseType: "arraybuffer",
+          });
 
-      // reader.onloadend = (): void => {
-      //   ;
-      //   if (reader["result"]) {
-      // const audio = new window.Audio(reader["result"] as string);
-      // const source = audioContext.createMediaElementSource(audio);
-      // console.log(source);
-      // source.connect(audioContext.destination);
-      // source.audio.play();
-      //   }
-      // };
+          gainNode.gain.value = 1;
+          audioContext.decodeAudioData(
+            response["data"],
+            (buffer: AudioBuffer) => {
+              const source = audioContext.createBufferSource();
+
+              source.buffer = buffer;
+              source.connect(audioContext.destination);
+              source.start();
+              source.onended = () => {
+                togglePlaying(false);
+              };
+            }
+          );
+        }
+      };
 
       if (response && response["data"]) {
         audioContext.decodeAudioData(
           response["data"],
           (buffer: AudioBuffer): void => {
-            const source = audioContext.createBufferSource();
+            const wav = bufferToWav(buffer);
+            const blob: Blob = new window.Blob([new DataView(wav)], {
+              type: "audio/wav",
+            });
 
-            source.buffer = buffer;
-            source.connect(audioContext.destination);
-            //@ts-ignore
-            source.start ? source.start(0) : source.noteOn(0);
-
-            source.onended = () => {
-              togglePlaying(false);
-            };
-            // const blob: Blob = new window.Blob([new DataView(wav)], {
-            //   type: "audio/wav",
-            // });
-
-            // reader.readAsDataURL(blob);
+            reader.readAsDataURL(blob);
           }
         );
       }
--- src/components/Words/List/Item/types.ts
+++ src/components/Words/List/Item/types.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    webkitAudioContext: typeof AudioContext;
+  }
+}
+
 export interface TWord {
   audioLink: string;
   word: string;
